88d7ed5a0f18c271e1b4b49d7b213db3c13de74b
Milky066 <sahassapol@gmail.com>
2022-08-17 15:35:58 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi mom!</Text>
+      <Text>I'm MIl</Text>
       <StatusBar style="auto" />
     </View>
   );
