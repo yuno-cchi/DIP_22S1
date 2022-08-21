b7d2c73ca4537e472d8301c9997d4e0b63754c6f
Milky066 <sahassapol@gmail.com>
2022-08-21 10:16:06 +0800

    Add a barebone map
    -Implement Map feature using Apple Map
    -Yet to configure for Google(To do later)
    -The map covers the entire screen(Adjust later)

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from "react-native-maps";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.innerText}>Hello!</Text>
       <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        //specify our coordinates, ask for user location
+        initialRegion={{
+          latitude: 1.3483,
+          longitude: 103.6831,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+      />
     </View>
   );
 }
@@ -21,5 +32,8 @@ const styles = StyleSheet.create({
   innerText: {
     color: 'white',
     fontSize: 80
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
   }
 });
