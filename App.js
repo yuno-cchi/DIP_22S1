6129eb0c661a6c3664fc1f41bc0999800cb59cf6
Milky066 <sahassapol@gmail.com>
2022-08-27 19:19:21 +0800

    Revert "Theme and stuffs"
    This reverts commit f57ac8766c6dd0a97b699389b920f9128306cd37.

--- App.js
+++ App.js
@@ -1,72 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
-import MapView from "react-native-maps";
+import { StyleSheet, Text, View } from 'react-native';
 
-import appPalette from "./dist/lib/appPalette";
-//Dont bloody import Button from react-native-web
-
-const Separator = () => (
-  <View style={styles.separator} />
-);
-
-function App() {
+export default function App() {
   return (
     <View style={styles.container}>
-
-      <View style={styles.mapContainer}>
-        <MapView style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
-      <View style={{ flex: 1, flexDirection: "column", justifyContent: "space-evenly", alignItems: "flex-start" }}>
-        <Button title="Click me " onPress={() => console.log("Pressed")} />
-        <Button title="Click me 2" onPress={() => console.log("2")} />
-      </View>
+      <Text>I'm MIlasodhasod</Text>
+      <Text>Hello</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-function getUserLocation() {
-  console.log("Next is pressed")
-}
-
-
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'north',
-    flexDirection: 'column'
-  },
-  button: {
-    width: '10%',
-    height: '5%'
-  },
-  innerText: {
-    color: 'white',
-    fontSize: 80
-  },
-  mapContainer: {
-    width: '100%',
-    height: '75%',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+    justifyContent: 'center',
+
   },
 });
-
-export default App;
