3f051fdc1797e75ac465fcb1f184582fb13b448c
Sahassapon Manussawin <sahassapol@gmail.com>
2022-09-07 14:25:39 +0800

    Backend add

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+/*
+Server.js = the Express js file that links this project to the dabatase
+
+
+
+
+*/
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+
+//TODO: to add once u have more routes 
+const userRouter = require('./routes/user')
+const driverRouter = require('./routes/driverdetails')
+const rideRouter = require('./routes/ride')
+
+//TODO: add accordingly based on the router variable u declared
+app.use('/user', userRouter);
+app.use('/driverdetails', driverRouter);
+app.use('/ride', rideRouter);
+
+
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port: ${port}`);
+});
+
+
