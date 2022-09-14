863f4c870e324ff1ac062ab14a42839461d7afa4
Ksnaeh <65495468+Ksnaeh@users.noreply.github.com>
2022-09-14 16:36:42 +0800

    TODO: solve the async await issue ltr

--- Screen/CheeHeanDomain.js
+++ Screen/CheeHeanDomain.js
@@ -5,6 +5,60 @@ import MapView from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import { color } from '../Config/Color';
 import AppButton from '../Components/AppButton';
+import { Logs } from 'expo'
+import axios from 'axios';
+
+Logs.enableExpoCliLogging();
+
+//TODO: insert data and userid for its args
+
+var cheeHeanData = [];
+
+async function callRideDetails(){
+
+    console.log("loading");
+
+
+    const resp = await axios.get('http://secret-caverns-21869.herokuapp.com/ride');
+    ridedata = resp.data;
+    console.log(ridedata);
+
+    console.log(ridedata[0]._id);
+
+    //TODO: do a for loop to compare userid and data 
+    console.log(ridedata[0].start);
+
+    //TODO: replace '0' with the id
+    const start = ridedata[0].start;
+    const destination = ridedata[0].destination;
+
+    const waypoints = ridedata[0].stopPoint;
+
+    var xArray = []
+
+    console.log(waypoints);
+
+    console.log("start point lat: " + start.latitude);
+    console.log("start point long: " + start.longitude);
+
+    for(var x = 0; x < waypoints.latitude.length; x++){
+        console.log("\n");
+        console.log(x + ": " + waypoints.longitude[x]);
+        console.log(x + ": " + waypoints.latitude[x]);
+
+        
+        xArray.push({key: x, userID: ridedata[0]._id, latitude: parseFloat(waypoints.latitude[x]), longitude: parseFloat(waypoints.longitude[x]), markerColor: color.red, travelDate: "2022-09-14"})
+
+    }
+
+    console.log("\n");
+    console.log("end point lat: " + destination.latitude);
+    console.log("end point long: " + destination.longitude);
+
+    return await xArray;
+}
+
+cheeHeanData = callRideDetails();
 
 const dummyRoute = [
     {
@@ -29,8 +83,6 @@ const dummyRoute = [
     }
 ]
 
-const cheeHeanData = [];
-
 const setMarkerColor = (markerArray, key, color1, color2) => {
     for (let index = 0; index < markerArray.length; index++) {
         if (markerArray[index].key === key) {
@@ -50,10 +102,19 @@ const setMarkerColor = (markerArray, key, color1, color2) => {
 const GOOGLE_MAPS_APIKEY = 'AIzaSyBYDEKY12RzWyP0ACQEpgsr4up2w3CjH88';
 const dummyDesti = { latitude: 1.3295337383405326, longitude: 103.86303846371023 }
 
-export default function DriverMapScreen() {
+export default function CheeHeanDomain() {
     const [routeVisible, setRouteVisible] = useState(false);
     const [desti, setDesti] = useState();
-    const [loadedRoute, setMarker] = useState(dummyRoute);
+    (async () => {
+        console.log("hi");
+        await callRideDetails();
+        console.log(await cheeHeanData);
+        const [loadedRoute, setMarker] = useState(cheeHeanData);
+    })()
+    // console.log("hu");
+    // console.log(cheeHeanData);
+    // const [loadedRoute, setMarker] = useState(cheeHeanData);
+
 
     return (
         <View style={styles.container}>
