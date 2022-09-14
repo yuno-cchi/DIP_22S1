86266164475ffa038a7580796d4b12387792c7a7
Sahassapon Manussawin <sahassapol@gmail.com>
2022-09-14 16:37:01 +0800

    Update

--- Screen/DriverMapScreen.js
+++ Screen/DriverMapScreen.js
@@ -29,6 +29,12 @@ const dummyRoute = [
     }
 ]
 
+const key = 3;
+
+const startCoordinateExist = false;
+
+const cheeHeanData = [];
+
 const setMarkerColor = (markerArray, key, color1, color2) => {
     for (let index = 0; index < markerArray.length; index++) {
         if (markerArray[index].key === key) {
@@ -52,6 +58,11 @@ export default function DriverMapScreen() {
     const [routeVisible, setRouteVisible] = useState(false);
     const [desti, setDesti] = useState();
     const [loadedRoute, setMarker] = useState(dummyRoute);
+    const [buttonColor, setButtonColor] = useState(color.primary);
+    const [startCoordinate, setStartCoordinate] = useState();
+    const [destinationCoordinate, setDestinationCoordinate] = useState();
+    const [startEndCoordinate, setStartEndCoordinate] = useState();
+    //use onMarkerSelect inside the MapView
 
     return (
         <View style={styles.container}>
@@ -59,17 +70,28 @@ export default function DriverMapScreen() {
                 style={styles.container}
                 showsUserLocation={true}
                 showsPointsOfInterest={true}
-                showsMyLocationButton={true}
+                onLongPress={(e) => {
+                    const { latitude, longitude } = e.nativeEvent.coordinate
+                    setStartCoordinate({
+                        key: key,
+                        latitude: latitude,
+                        longitude: longitude,
+                        markerColor: color.danger
+                    })
+                    console.log(latitude, longitude)
+                }}
             >
                 <MapViewDirections
-                    origin={{ latitude: 1.3625511761253166, longitude: 103.70878307869019 }}
+                    origin={startCoordinate}
                     apikey={GOOGLE_MAPS_APIKEY}
                     optimizeWaypoints={true}
                     strokeWidth={12}
                     strokeColor={color.red}
                     destination={routeVisible ? desti : null}
                 />
+
                 {loadedRoute.map(({ key, latitude, longitude, markerColor }) => (
+
                     <Marker
                         key={key}
                         coordinate={{ 'latitude': latitude, 'longitude': longitude }}
@@ -86,8 +108,16 @@ export default function DriverMapScreen() {
                         }}
                     />))}
 
+
             </MapView>
-            <AppButton style={styles.showRoute} title="Route" onPress={() => setRouteVisible(!routeVisible)} />
+            <View style={styles.tab}>
+                <AppButton style={styles.showRoute} thisColor={buttonColor} title="Route" onPress={() => {
+                    setRouteVisible(!routeVisible)
+                    setButtonColor(buttonColor === color.primary ? color.secondary : color.primary)
+                }} />
+                <Marker draggable />
+            </View>
+
         </View>
 
     );
@@ -102,5 +132,12 @@ const styles = StyleSheet.create({
         bottom: 40,
         width: 200,
         right: 20
+    },
+    tab: {
+        position: 'absolute',
+        backgroundColor: color.lightGray,
+        width: '100%',
+        height: 200,
+        bottom: 0
     }
 })
