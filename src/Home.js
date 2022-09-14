c25b71b89d702c8a63beee0c29b7e3d9164ca068
yuno-cchi <fakeymcnoexist@gmail.com>
2022-09-14 16:36:19 +0800

    idk

--- src/Home.js
+++
@@ -1,6 +0,0 @@
-import React, {useEffect, useState} from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-
-
-
--- src/homeStack.js
+++
@@ -1,4 +0,0 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "./Home";
-
