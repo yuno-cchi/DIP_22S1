635ed0f5554ed38d41eaef302bf85d12ca18162f
yuwxnn <73411827+yuwxnn@users.noreply.github.com>
2022-10-12 16:45:25 +0800

    remove unnecessary import

--- Components/AppText.js
+++ Components/AppText.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Text, StyleSheet, Platform } from 'react-native';
-import * as Font from 'expo-font';
 
 export default function AppText({ children, style }) {
     return (
