c488a76598ca284de1fcc7b09abc5e49d932fa31
MJ <i200002@e.ntu.edu.sg>
2022-10-12 14:22:25 +0800

    apptabnavigator

---
+++ Components/AppTabNavigator.js
@@ -0,0 +1,3 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
