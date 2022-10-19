d1f14d928e221c3ddb3f37fa5ebeba0af7b4e5c0
MJ <i200002@e.ntu.edu.sg>
2022-10-19 16:17:11 +0800

    change btn

--- Components/AppButton.js
+++ Components/AppButton.js
@@ -1,36 +1,43 @@
 import React from "react";
-import { View, StyleSheet, Text, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
-import { color } from '../Config/Color';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from "react-native";
+import { color } from "../Config/Color";
 import Icon from "./Icon";
 
-
-export default function AppButton({ title, onPress, style, iconName, textSize = 20 }) {
-    return (
-        <TouchableOpacity style={[[styles.button, style]]} onPress={onPress}>
-            {iconName && <Icon name={iconName} />}
-            <Text style={[styles.title, { fontSize: textSize }]}>
-                {title}
-            </Text>
-        </TouchableOpacity>
-
-    );
+export default function AppButton({
+  title,
+  onPress,
+  style,
+  iconName,
+  textSize = 20,
+}) {
+  return (
+    <TouchableOpacity style={[[styles.button, style]]} onPress={onPress}>
+      {iconName && <Icon name={iconName} />}
+      <Text style={[styles.title, { fontSize: textSize }]}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: color.primary,
-        borderRadius: 25,
-        alignContent: 'center',
-        justifyContent: 'center',
-        padding: 15,
-        width: '100%',
-        marginVertical: 10
-    },
-    title: {
-        color: color.white,
-        fontWeight: "bold",
-        textTransform: 'uppercase',
-        alignSelf: 'center'
-    }
-})
-
+  button: {
+    backgroundColor: color.primary,
+    borderRadius: 25,
+    alignContent: "center",
+    justifyContent: "center",
+    padding: 15,
+    width: "100%",
+    marginVertical: 15,
+  },
+  title: {
+    color: color.white,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    alignSelf: "center",
+  },
+});
