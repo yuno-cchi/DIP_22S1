2b3ba9963a80b5924e261c61f1802f08764bdd4b
Sahassapon Manussawin <sahassapol@gmail.com>
2022-10-22 20:01:55 +0800

    Update DrivingNavigationScreen.js

--- Screen/DrivingNavigationScreen.js
+++ Screen/DrivingNavigationScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
-import MapView, { Overlay } from 'react-native-maps';
+import MapView, { Marker, Overlay } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import * as Location from 'expo-location';
 import AppButton from '../Components/AppButton';
@@ -24,19 +24,52 @@ async function getLiveLocation() {
     }
 
     let location = await Location.getCurrentPositionAsync();
-    let heading = await Location.getHeadingAsync();
+    let heading1 = await Location.getHeadingAsync();
 
     const userCoordinate = { latitude: location.coords.latitude, longitude: location.coords.longitude };
     console.log("User coordinates:", userCoordinate);
-    console.log("User heading: ", heading)
+    console.log("User heading: ", heading1)
+    let CameraObj = {
+        center: {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude
+        },
+        pitch: 0,
+        altitude: null,
+        heading: heading1,
+        zoom: 1
+    }
+    return CameraObj
+}
 
+let Camera = {
+    center: {
+        latitude: null,
+        longitude: null,
+    },
+    pitch: null,
+    heading: null,
+
+    // Only on iOS MapKit, in meters. The property is ignored by Google Maps.
+    altitude: null,
+
+    // Only when using Google Maps.
+    zoom: null
 }
 
 
 export default function DrivingNavigationScreen({ navigation, route }) {
+    let userLocation
+    let dataObj = route.params
+    useEffect(() => {
+        userLocation = getLiveLocation();
+
+    }, []);
     return (
         <View style={styles.container}>
             <MapView
+                provider={null}
+
                 ref={thisObj => mapView = thisObj}
                 style={{ ...StyleSheet.absoluteFill }}
                 showsBuildings={true}
@@ -44,17 +77,19 @@ export default function DrivingNavigationScreen({ navigation, route }) {
                 showsUserLocation={true}
                 showsPointsOfInterest={true}
                 showsCompass={true}
-                userLocationPriority={'high'}
-                userLocationAnnotationTitle="hey"
-            >
-                <Overlay />
-                <MapViewDirections
+                userLocationPriority={'low'}
+                followsUserLocation={true}
 
-                    apikey={GOOGLE_API_KEY}
-                    mode='DRIVING'
-                />
+
+            >
+
+
                 <AppButton
-                    onPress={() => { getLiveLocation() }}
+                    onPress={() => {
+                        console.log("Getting camera")
+                        console.log(mapView.getCamera())
+                        //mapView.animateCamera(userLocation, { duration: 100 })
+                    }}
                 />
 
             </MapView>
